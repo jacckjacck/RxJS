42ab9c3f2742925a14e3fc341e9ac90782f5bcfb
jorge gomez <jorge.gomez@tumesadeayuda.com>
2019-11-04 10:19:53 -0500

    Using throttle time function to skip multiple button clicks

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var button = document.querySelector('button');
 
-//Using RxJS for handling click button event
-Rx.Observable.fromEvent(button, 'click').subscribe(
-  (event) => console.log(event)
-);
+//Using RxJS for handling click button event, only each one second
+Rx.Observable.fromEvent(button, 'click')
+    .throttleTime(1000)
+    .subscribe(
+        (event) => console.log('button clicked')
+    );
