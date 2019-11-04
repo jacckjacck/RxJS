a73004806373eece041b2c0fa3a4649ff8fcb577
jorge gomez <jorge.gomez@tumesadeayuda.com>
2019-11-04 12:02:08 -0500

    using the map operator

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-var button = document.querySelector('button');
+//using the interval built-in RxJS observable
+// emiting a tick every second
+var observale = Rx.Observable.interval(1000);
 
-//Using RxJS for handling click button event, only each one second
-Rx.Observable.fromEvent(button, 'click')
-    .throttleTime(1000)
-    .subscribe(
-        (event) => console.log('button clicked')
-    );
+var observer = {
+    next: (value) => {
+        console.log(value);
+    }
+};
+
+/*using map operator for transforming each value that is getting from the observable
+ the map operator returns a new observable and it's passed to the subscribe method
+*/
+observale
+    .map( (value) => {
+        return `New value is ${value * 1.25}`;
+    })
+    .subscribe(observer); 
