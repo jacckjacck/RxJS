feff49f738d7dd06b8a65e13bc5b4ad5507efb5d
jorge gomez <jorge.gomez@tumesadeayuda.com>
2019-11-05 17:00:51 -0500

    pluck operator example

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
+//Get input instance from DOM
+var input = document.querySelector('input');
+
 //Creates the observable from input event
-var observable = Rx.Observable.of(1,2,3,4,5);
-
-
-/**
- * reduce operator apply a function for a entry stream sequentially
- * when input stream completes, it returns the acummulated value
- * */
-observable
-    .reduce( (total, currentValue) => {
-        return total+currentValue;
-    },0)
-    .subscribe({
-        next: (value) => {
-            console.log(value)
-        }
-    });
-
+var observable = Rx.Observable.fromEvent(input, 'input');
 
 /**
- * scan operator apply a function for a entry stream sequentially
- * and it returns each emitted value in an accumulative way
- * */
+ * pluck is applied to objects, and it allow to access to the 
+ * object properties, something similar to map but more readable
+ */
 observable
-    .scan( (total, currentValue) => {
-        return total+currentValue;
-    },0)
+    .pluck('target', 'value')
+    .debounceTime(500)
+    .distinctUntilChanged() 
     .subscribe({
         next: (value) => {
-            console.log(value)
+            console.log(value);
         }
     });
