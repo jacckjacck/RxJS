4b3978d74dad29780079033aa7991e6b7f399438
jorge gomez <jorge.gomez@tumesadeayuda.com>
2019-11-05 14:08:48 -0500

    adding distinctuntilchange operator

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ var observable = Rx.Observable.fromEvent(input, 'input');
  * debounceTime allow to emit values from the observable only 
  * after a specific time without emitting event from source
  * 
+ * disctinctUntilChanged only emits values if data has some change
  */
 observable
+    .map( event$ => event$.target.value)
     .debounceTime(4000)
+    .distinctUntilChanged() 
     .subscribe({
-        next: (event$) => {
-            console.log(event$.target.value);
+        next: (value) => {
+            console.log(value);
         }
     });
