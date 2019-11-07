8b6588e537c9d811f224b11bc8a09f07479a47fa
jorge gomez <jorge.gomez@tumesadeayuda.com>
2019-11-06 21:47:42 -0500

    switchMap operator example

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-//Get input instance from DOM
-var input1 = document.querySelector('#input1');
-var input2 = document.querySelector('#input2');
+//Get button instance from DOM
+var button = document.querySelector('button');
 
 var span = document.querySelector('span');
 
 //Creates the observable from input event
-var observable1 = Rx.Observable.fromEvent(input1, 'input');
-var observable2 = Rx.Observable.fromEvent(input2, 'input');
+var observable1 = Rx.Observable.fromEvent(button, 'click');
+var observable2 = Rx.Observable.interval(1000);
 
 
 /**
  * mergeMap operator allow us to merge two observables
+ * but each new stream that is emitted, the previous stream is cancelled
  */
-
-observable1.mergeMap(
-    (event1) => {
-        return observable2.map( (event2) => 
-            event1.target.value + ' '+  event2.target.value
-        )
-    }
-).subscribe( (combinedValue) => {
-    span.textContent = combinedValue;
-});
+ observable1.switchMap(
+     event$ => {
+         return observable2;
+     }
+ ).subscribe(
+     value => console.log(value)
+ );
